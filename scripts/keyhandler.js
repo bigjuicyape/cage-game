f7fbd1989ddad313a114f8d30c33b33bff357557
BIGJUICYAPE <113376487+bigjuicyape@users.noreply.github.com>
2022-10-28 11:22:51 -0600

    e

--- scripts/keyhandler.js
+++ scripts/keyhandler.js
@@ -1,32 +1,76 @@
 addEventListener( "keydown", doKeyDown, true);
+addEventListener( "keyup", doKeyup, true);
 
 function doKeyDown(e) {
 //d
-if ( e.keyCode == 68 ) {
-    vx = 10;
+if ( e.keyCode == 68) {
+    vxr = 4;
     ctx.fillStyle = "white"
     ctx.fillRect(0, 0, cnvwidth, cnvheight);
-    ctx.drawImage(htmlimg, x, y, 50, 25);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
 }
 //a
 if ( e.keyCode == 65 ) {
-    vx = - 10;
+    vxl = - 4;
     ctx.fillStyle = "white"
     ctx.fillRect(0, 0, cnvwidth, cnvheight);
-    ctx.drawImage(htmlimg, x, y, 50, 25);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
 }
 // w
 if ( e.keyCode == 87 ) {
-    vy = - 10;
+    vyu = - 2;
     ctx.fillStyle = "white"
     ctx.fillRect(0, 0, cnvwidth, cnvheight);
-    ctx.drawImage(htmlimg, x, y, 50, 25);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
 }
 // s
 if ( e.keyCode == 83 ) {
-    vy = + 10;
+    vyd = + 2;
     ctx.fillStyle = "white"
     ctx.fillRect(0, 0, cnvwidth, cnvheight);
-    ctx.drawImage(htmlimg, x, y, 50, 25);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
 }
+}
+
+
+function doKeyup(e) { 
+//d
+if ( e.keyCode == 68 ) {
+    vxr = 0;
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, cnvwidth, cnvheight);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
+}
+//a
+if ( e.keyCode == 65 ) {
+    vxl = 0;
+    x += vxl
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, cnvwidth, cnvheight);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
+}
+// w
+if ( e.keyCode == 87 ) {
+    vyu = 0;
+    ctx.fillStyle = "white"
+    ctx.drawImage(htmlcage, x, y, 50, 30);
+    ctx.fillRect(0, 0, cnvwidth, cnvheight);
+}
+// s
+if ( e.keyCode == 83 ) {
+    vyd = 0;
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, cnvwidth, cnvheight);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
+}
+}
+setInterval(update, 0.00001);
+function update(){
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, cnvwidth, cnvheight);
+    ctx.drawImage(htmlcage, x, y, 50, 30);
+    x += vxl
+    x += vxr
+    y += vyu
+    y += vyd
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
 let htmlimg = document.getElementById("goblin");
+let htmlcage = document.getElementById("cage");
 let cnvheight = 600;
 let cnvwidth = 600;
-var x = 125;
-var y = 60;
+let x = 125;
+let y = 60;
+let vy = 0;
+let vx = 0;
+
 ctx.drawImage(htmlimg, x, y, 50, 25);
 
 window.addEventListener( "keypress", doKeyDown, false );
@@ -15,8 +19,10 @@ function reset() {
     ctx.fillRect(0, 0, cnvwidth, cnvheight);
     x = 125
     y - 60
-    ctx.drawImage(htmlimg, x, y, 50, 25);
+    ctx.drawImage(htmlcage, x, y, 50, 25);
 }
+
+
 
 // 119 = w 
 // 97 = a 
