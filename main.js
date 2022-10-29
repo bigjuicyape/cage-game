3495e1f3015ef5fe0d711af7b2205fa13ba2d346
BIGJUICYAPE <113376487+bigjuicyape@users.noreply.github.com>
2022-10-28 21:21:03 -0600

    cage

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-const c = document.getElementById('myCanvas');
-const ctx = c.getContext('2d');
-let up = document.getElementById('up');
-let down = document.getElementById('down');
-let left = document.getElementById('left');
-let right = document.getElementById('right');
-let bg = document.getElementById('bg');
-let htmlcage = document.getElementById('cage');
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+let up = document.getElementById("up");
+let down = document.getElementById("down");
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+let bg = document.getElementById("bg");
+let cage = document.getElementById("cage");
 let vx = 0;
 let vy = 0;
+let xcage = 450 * Math.random();
+let ycage = 450 * Math.random();
 let wDown = false;
 let aDown = false;
 let sDown = false;
@@ -20,95 +22,93 @@ c.height = 600;
 let x = c.width / 2 - 150 / 2;
 let y = c.height / 2 - 100 / 2;
 
-document.addEventListener('keydown', keydown);
-document.addEventListener('keyup', keyup);
+document.addEventListener("keydown", keydown);
+document.addEventListener("keyup", keyup);
 
 function keydown(event) {
-   if (event.key == 'a') {
-      aDown = true;
-   }
-   if (event.key == 'd') {
-      dDown = true;
-   }
-   if (event.key == 'w') {
-      wDown = true;
-   }
-   if (event.key == 's') {
-      sDown = true;
-   }
+  if (event.key == "a") {
+    aDown = true;
+  }
+  if (event.key == "d") {
+    dDown = true;
+  }
+  if (event.key == "w") {
+    wDown = true;
+  }
+  if (event.key == "s") {
+    sDown = true;
+  }
 }
 
 function keyup(event) {
-   if (event.key == 'a') {
-      aDown = false;
-   }
-   if (event.key == 'd') {
-      dDown = false;
-   }
-   if (event.key == 'w') {
-      wDown = false;
-   }
-   if (event.key == 's') {
-      sDown = false;
-   }
+  if (event.key == "a") {
+    aDown = false;
+  }
+  if (event.key == "d") {
+    dDown = false;
+  }
+  if (event.key == "w") {
+    wDown = false;
+  }
+  if (event.key == "s") {
+    sDown = false;
+  }
 }
 
 ctx.drawImage(down, x, y, 50, 30);
 
 function reset() {
-   x = c.width / 2 - 150 / 2;
-   y = c.height / 2 - 100 / 2;
-   ctx.drawImage(down, x, y, 50, 30);
+  xcage = 450 * Math.random();
+  ycage = 450 * Math.random();
 }
 
 requestAnimationFrame(loop);
 
 function loop() {
-   ctx.drawImage(bg, 0, 0, c.width, c.height);
+  ctx.drawImage(bg, 0, 0, c.width, c.height);
 
-   if (aDown) {
-      vx = -4;
-      image = left;
-   } else if (dDown) {
-      vx = 4;
-      image = right;
-   } else {
-      vx = 0;
-   }
+  if (aDown) {
+    vx = -4;
+    image = left;
+  } else if (dDown) {
+    vx = 4;
+    image = right;
+  } else {
+    vx = 0;
+  }
 
-   if (wDown) {
-      vy = -4;
-      image = up;
-   } else if (sDown) {
-      vy = 4;
-      image = down;
-   } else {
-      vy = 0;
-   }
+  if (wDown) {
+    vy = -4;
+    image = up;
+  } else if (sDown) {
+    vy = 4;
+    image = down;
+  } else {
+    vy = 0;
+  }
 
-   if ((aDown || dDown) && (wDown || sDown)) {
-      vx += -Math.sign(vx) * Math.sqrt(2);
-      vy += -Math.sign(vy) * Math.sqrt(2);
-   }
+  if ((aDown || dDown) && (wDown || sDown)) {
+    vx += -Math.sign(vx) * Math.sqrt(2);
+    vy += -Math.sign(vy) * Math.sqrt(2);
+  }
 
-   x += vx;
-   y += vy;
+  x += vx;
+  y += vy;
 
-   if (x < -20) {
-      x = -20;
-   } else if (x > c.width - 130) {
-      x = c.width - 130;
-   }
+  if (x < -20) {
+    x = -20;
+  } else if (x > c.width - 130) {
+    x = c.width - 130;
+  }
 
-   if (y < 0) {
-      y = 0;
-   } else if (y > c.height - 90) {
-      y = c.width - 90;
-   }
-
-   ctx.drawImage(image, x, y, 150, 100);
-
-   requestAnimationFrame(loop);
+  if (y < 0) {
+    y = 0;
+  } else if (y > c.height - 90) {
+    y = c.width - 90;
+  }
+  ctx.drawImage(image, x, y, 150, 100);
+  ctx.drawImage(cage, xcage, ycage, 200, 200);
+  requestAnimationFrame(loop);
 }
 
 // 119 = w
